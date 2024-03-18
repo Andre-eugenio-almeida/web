7129dac2c331dc77ce7fade506f3a4a0cfaee1aa
Andre-eugenio-almeida <almeida.ae@outlook.com>
2024-03-18 15:24:38 -0300

    upgrade components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./sharepage/navbar/navbar.component";
+import { FooterComponent } from "./sharepage/footer/footer.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, NavbarComponent]
+    imports: [RouterOutlet, NavbarComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'web';
